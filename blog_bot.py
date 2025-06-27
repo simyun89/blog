8b0f387eefa5a84667f01cb5f9707372beb8613a
simyun89 <sdgksdh@gmail.com>
2025-06-27 16:12:53 +0900

    Update blog_bot.py

--- blog_bot.py
+++ blog_bot.py
@@ -110,7 +110,7 @@ freq_side_df = pd.DataFrame({
 })
 
 # 2-e) GPT 이슈 클러스터링
-enc = tiktoken.encoding_for_model("gpt-4o-mini")
+enc = tiktoken.encoding_for_model("gpt-4.1")
 def num_tokens(txt): return len(enc.encode(txt))
 
 def gpt_cluster(title_list, label):
